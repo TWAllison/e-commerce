189925a1d11d6f763144ea53953b856bdd2d778f
TWAllison <tylerallison55@gmail.com>
2021-12-27 06:11:07 -0700

    create relationships and forignkeys in index.js, check paths are correct

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+product.belongsTo(Category, {
+  forignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  forignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
