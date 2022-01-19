61f32ecb00d1e70b27e12fcd8e96366601de0bed
TWAllison <tylerallison55@gmail.com>
2022-01-19 02:49:31 -0700

    typo fix

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../config/connection");
 
-class Category extends Model { }
+class Category extends Model {}
 
 Category.init(
   {
@@ -12,13 +12,10 @@ Category.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    cate
-    
-    
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
   },
   {
     sequelize,
